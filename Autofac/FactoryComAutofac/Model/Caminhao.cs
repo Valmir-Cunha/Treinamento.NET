5dda4eb8b6dded144ba7add4d03c8b3f11a79adb
Valmir-Cunha <vinicius.git36@gmail.com>
2022-10-07 08:57:04 -0300

    Update

---
+++ Autofac/FactoryComAutofac/Model/Caminhao.cs
@@ -0,0 +1,14 @@
+﻿namespace FactoryMethod.Model
+{
+    public class Caminhao : Transporte
+    {
+        public Caminhao(string descricao, int fabricacao) : base(descricao, fabricacao)
+        {
+        }
+
+        public override void Entregar(string pacote)
+        {
+            Console.WriteLine($"O pacote {pacote} será transportado por um caminhao modelo {_descricao}");
+        }
+    }
+}
--- Autofac/FactoryComAutofac/Program.cs
+++ Autofac/FactoryComAutofac/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Autofac;
+using FactoryMethod.Factories;
+using FactoryMethod.Model;
+
+namespace FactoryComAutofac
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Registros.RegistrarDependencias();
+            TransporteFactory transporteFactory;
+            Transporte transporte;
+            string pacote = "N° 2893741892";
+            Console.WriteLine("Digite o código do tipo de transporte que deseja utilizar no envio:");
+            Console.WriteLine("1 - Caminhão | 2 - Avião");
+            string codigo = Console.ReadLine();
+            using (var containerFilho = Registros.CarregarContainer())
+            {
+                switch (codigo)
+                {
+                    case "1":
+                        transporteFactory = containerFilho.Resolve<CaminhaoFactory>();
+                        transporte = transporteFactory.CriarTransporte("Modelo X", 2015);
+                        transporte.Entregar(pacote);
+                        transporteFactory._serviceSimulado.FazAlgo();
+                        break;
+                    case "2":
+                        transporteFactory = containerFilho.Resolve<AviaoFactory>();
+                        transporte = transporteFactory.CriarTransporte("Modelo Y", 2016);
+                        transporte.Entregar(pacote);
+                        transporteFactory._serviceSimulado.FazAlgo();
+                        break;
+                }
+                
+            }
+        }
+    }
+}
